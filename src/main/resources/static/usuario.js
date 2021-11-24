db6b4329e77cc22f517f19cf62d6a58931e63cab
gabrieDF <gabriel.d.feitosa@hotmail.com>
2021-11-24 01:32:37 -0300

    criando um repositorio

---
+++ src/main/resources/static/usuario.js
@@ -0,0 +1,32 @@
+function logar(){
+	
+    var objeto = {
+        "email" : document.getElementById("txtemail").value,
+        "senha" : document.getElementById("txtsenha").value
+    }
+
+    var cabecalho = {
+        method: "POST",
+        body: JSON.stringify(objeto),
+        headers:{
+            "Content-type":"application/json"
+        }
+    }
+
+    fetch("http://localhost:8080/login", cabecalho)
+        .then(res => res.json())
+        .then(res => {
+            localStorage.setItem("logado",JSON.stringify(res));
+          //window.location="index";
+			document.getElementById("id").innerText = res.id
+			document.getElementById("nome").innerText = res.nome
+			document.getElementById("senha").innerText = res.senha
+			document.getElementById("email").innerText = res.email
+        })
+        .catch(err => {
+			console.log(err);
+            window.alert("Deu ruim");
+        });
+    
+
+}
